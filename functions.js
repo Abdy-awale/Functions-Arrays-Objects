b60dfb291825483b12762dc9996198af1c638f27
abdisalan awale <46216893+Abdy-awale@users.noreply.github.com>
2022-04-10 16:33:42 +1000

    FUNCTION-ARRAY-OBJECT

--- functions.js
+++ functions.js
@@ -1,4 +1,3 @@
-
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: Multiply 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 /*
 
@@ -8,13 +7,11 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-    /*add your code here*/
-  }
-
-
-/*
-
+function multiply(a, b) {
+	/*add your code here*/
+	return a * b;
+}
+multiply(2, 3);
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: Age in Cat years 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -25,81 +22,160 @@ Do the following:
    3. Return the newly calculated age
 */
 
-function catYears(/*add your code here*/){
-    /*add your code here*/
+function catYears(age) {
+	/*add your code here*/
+	const catYears = age * 7;
+	return catYears;
 }
-
-
-
+catYears(1);
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: Convert to Arrow Function  🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-
-/*
-
-------------
-function myFunction() {
-console.log("Function was invoked!");
+// ------------
+const myFunction = () => {
+	console.log('Function was invoked!');
 };
 
 myFunction();
-----------------
 
-let anotherFunction = function (param) {
-  return param;
+// ----------------
+
+let anotherFunction = (param) => {
+	return param;
 };
 
-anotherFunction("Example");
+anotherFunction('Example');
 
----------------
+// ---------------
 
-let add = function (param1, param2) {
-  return param1 + param2;
+let add = (param1, param2) => {
+	return param1 + param2;
 };
 
-add(1,2);
-
-*/
-
-
+add(1, 2);
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: Rock, Paper, Scissors - Let's play against the computer! 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
-/*
-Create a global variable that randomly generates the computer's choice
-Use Math.random to determine the computers choice (Math.random gives a random number between 0 and 1)
-HINT: While you can complete this with only conditionals based on strings, it may help to equate choice to a number when using Math.random()
-Use the game function below to do the following:
-  1. Receive 2 parameters the user's choice and the computer's choice
-  2. Return whether the user won, lost, or tied based on these rules of the game described below - the strings returned need to match the strings below exactly.
-   - win should return "you win!"
-   - lose should return "you lose!"
-   - tie should return "it's a tie"
-  
-  RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
-  
-  HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
-*/
+// Create a global variable that randomly generates the computer's choice
+let computerChoice = Math.random() * 1;
+// Use Math.random to determine the computers choice (Math.random gives a random number between 0 and 1)
 
-function game(user, computer){
-    /*add your code here*/
-}
+// HINT: While you can complete this with only conditionals based on strings, it may help to equate choice to a number when using Math.random()
 
+// Use the game function below to do the following:
+const game = (userChoice) => {
+	// 1. Determine the computer's choice
+	if (computerChoice < 0.34) {
+		computerChoice = 'rock';
+	} else if (computerChoice <= 0.67) {
+		computerChoice = 'paper';
+	} else {
+		computerChoice = 'scissors';
+	}
+	// 2. Compare the user's choice to the computer's choice
+	// 3. Return the result of the game
+	if (userChoice === computerChoice) {
+		return 'The result is a tie!';
+	} else if (userChoice === 'rock') {
+		if (computerChoice === 'paper') {
+			return 'The computer won!';
+		} else {
+			return 'You won!';
+		}
+	} else if (userChoice === 'paper') {
+		if (computerChoice === 'scissors') {
+			return 'The computer won!';
+		} else {
+			return 'You won!';
+		}
+	} else if (userChoice === 'scissors') {
+		if (computerChoice === 'rock') {
+			return 'The computer won!';
+		} else {
+			return 'You won!';
+		}
+	} else {
+		return 'Please enter a valid choice!';
+	}
+};
+const userChoice = 'rock';
+game(userChoice);
 
+//   1. Receive 2 parameters the user's choice and the computer's choice
+//   2. Use the game function to determine the winner
+//   3. Return the winner
+const game2 = (userChoice, computerChoice) => {
+	// 1. Determine the computer's choice
+	if (computerChoice < 0.34) {
+		computerChoice = 'rock';
+	} else if (computerChoice <= 0.67) {
+		computerChoice = 'paper';
+	} else {
+		computerChoice = 'scissors';
+	}
+	// 2. Compare the user's choice to the computer's choice
+	// 3. Return the result of the game
+	if (userChoice === computerChoice) {
+		return 'The result is a tie!';
+	} else if (userChoice === 'rock') {
+		if (computerChoice === 'paper') {
+			return 'The computer won!';
+		} else {
+			return 'You won!';
+		}
+	} else if (userChoice === 'paper') {
+		if (computerChoice === 'scissors') {
+			return 'The computer won!';
+		} else {
+			return 'You won!';
+		}
+	} else if (userChoice === 'scissors') {
+		if (computerChoice === 'rock') {
+			return 'The computer won!';
+		} else {
+			return 'You won!';
+		}
+	} else {
+		return 'Please enter a valid choice!';
+	}
+};
+game2('rock', 0.5); // The computer won! 🤣
+//   2. Return whether the user won, lost, or tied based on these rules of the game described below - the strings returned need to match the strings below exactly.
+//    - win should return "you win!"
+//    - lose should return "you lose!"
+//    - tie should return "it's a tie"
 
+//   RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
+
+//   HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
+
+// function game(user, computer){
+//     /*add your code here*/
+
+// }
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
 // Let's Sing 99 Bottles of Soda on the Wall!
-/*
-Using the annoyingSong function below do the following:
-  1. Receive a starting number and start the count down from the number received 
-  2. At each invocation, it should RETURN this string (note: the tests are expecting the same string as below):
-      "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
-*/
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
-  }
+// Using the annoyingSong function below do the following:
+//   1. Receive a starting number and start the count down from the number received
+//   2. At each invocation, it should RETURN this string (note: the tests are expecting the same string as below):
+//       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
+const annoyingSong = (number) => {
+	/*add your code here*/
+	let song = '';
+	for (let i = number; i > 0; i--) {
+		song += `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${
+			i - 1
+		} bottles of soda on the wall.\n`;
+	}
+	return song;
+};
+console.log(annoyingSong(9));
+
+function annoyingSong(/*add your code here*/) {
+	/*add your code here*/
+}
--- objects.js
+++ objects.js
@@ -1,64 +1,133 @@
 // Let's get some practice writing a few objects for a new group of interns at a small business.
 
-// ==== Challenge 1: Writing Objects ==== 
-// HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
+// ==== Challenge 1: Writing Objects ====
+// HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender.
+// Create an object for each person in the company list:
 
 // 1, mmelloy0@psu.edu, Mitzi, F
 // 2, kdiben1@tinypic.com, Kennan, M
 // 3, kmummery2@wikimedia.org, Keven, M
 // 4, gmartinson3@illinois.edu, Gannie, M
 // 5, adaine5@samsung.com, Antonietta, F
+const company = [
+	{ id: 1, email: 'mmelloy0@psu.edu', firstName: 'Mitzi', gender: 'F' },
+	{ id: 2, email: 'kdiben1@tinypic.com', firstName: 'Kenan', gender: 'M' },
+	{ id: 3, email: 'kmummery2@wikimedia.org', firstName: 'Keven', gender: 'M' },
+	{
+		id: 4,
+		email: 'gmartinson3@illinois.edu',
+		firstName: 'Gannie',
+		gender: 'M',
+	},
+	{ id: 5, email: 'adaine5@samsung.com', firstName: 'Antonietta', gender: 'F' },
+];
 
-// Example format of an intern object: 1, examples@you.edu, Example, F
-const example = {
-    id: 0,
-    name: "Example",
-    email: "examples@you.edu",
-    gender: "F",
-  }
-  
-  // Write your intern objects here:
-  
-  
-  // ==== Challenge 2: Reading Object Data ==== 
-  // Once your objects are created, log out the following requests from HR into the console:
-  
-  // Mitzi's name
-  
-  // Kennan's ID
-  
-  // Keven's email
-  
-  // Gannie's name
-  
-  // Antonietta's Gender
-  
-  // ==== Challenge 3: Object Methods ==== 
-  // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-  // console.log(kennan.speak());
-  
-  // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-  //console.log(antonietta.multiplyNums(3,4));
-  
-  // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
-  
-  // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
-  
-  // 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
-  // 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
-  // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
-  // 4. Give each of the objects the ability to speak their names using the this keyword.
-  
-  const parent = {}
-  
-  // Log the parent object's name
-  
-  // Log the child's age
-  
-  // Log the name and age of the grandchild
-  
-  // Have the parent speak
-  
-  // Have the child speak
-  
-  // Have the grandchild speak
+// // Example format of an intern object: 1, examples@you.edu, Example, F
+// const example = {
+// 	id: 0,
+// 	name: 'Example',
+// 	email: 'examples@you.edu',
+// 	gender: 'F',
+// };
+
+// Write your intern objects here:
+
+// ==== Challenge 2: Reading Object Data ====
+// Once your objects are created, log out the following requests from HR into the console:
+
+// Mitzi's name
+company[0].firstName; // Mitzi
+
+// Kennan's ID
+company[1].id; // 2
+
+// Keven's email
+company[2].email; //?
+
+// Gannie's name
+company[3].firstName; // Gannie
+
+// Antonietta's Gender
+company[4].gender; //?
+
+// ==== Challenge 3: Object Methods ====
+// Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
+// console.log(kennan.speak());
+
+// Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
+//console.log(antonietta.multiplyNums(3,4));
+
+// === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
+
+// ==== Stretch Challenge: Nested Objects and the this keyword ====
+
+// 1. Create a parent object with properties for name and age.  Make the name Susan and the age 70.
+const parent = {
+	name: 'Susan',
+	age: 70,
+};
+
+// 2. Nest a child object in the parent object with name and age as well.  The name will be George and the age will be 50.
+const child = {
+	name: 'George',
+	age: 50,
+};
+// 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
+const grandchild = {
+	name: 'Sam',
+	age: 30,
+};
+// 4. Give each of the objects the ability to speak their names using the this keyword.
+// parent.speak(); // Susan
+// child.speak(); // George
+// grandchild.speak(); // Sam
+// ==== Challenge 4: Nested Objects ====
+const family = {
+	name: 'Susan',
+	age: 70,
+	child: {
+		name: 'George',
+		age: 50,
+		grandchild: {
+			name: 'Sam',
+			age: 30,
+			speak: function () {
+				console.log(`${this.name}`);
+			},
+		},
+		speak: function () {
+			console.log(this.name);
+		},
+	},
+	speak: function () {
+		console.log(`${this.name}`);
+	},
+};
+
+// 1. Log out the name of the child's grandchild.
+console.log(family.child.grandchild.name); // Sam
+// 2. Log out the age of the family.
+console.log(family.age); // 70
+// 3. Log out the name and age of the family's child.
+console.log(family.child.name, family.child.age); // George 50
+// 4. Log out the name and age of the family's child's grandchild.
+console.log(family.child.grandchild.name, family.child.grandchild.age); // Sam 30
+
+// const family = {};
+
+// Log the parent object's name
+console.log(family.name); // Susan
+
+// Log the child's age
+console.log(family.child.age); // 50
+
+// Log the name and age of the grandchild
+console.log(family.child.grandchild.name, family.child.grandchild.age); // Sam 30
+
+// Have the parent speak
+family.speak(); // Susan
+// Have the child speak
+family.child.speak(); // George
+
+// Have the grandchild speak
+family.child.grandchild.speak(); // Sam
